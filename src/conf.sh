7024426a9a336a448f65a9bfb704d03564a5bea9
yemiancheng <ymc.github@gmail.com>
2018-09-26 08:08:08 +0800

    feat(core): init src

---
+++ src/conf.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+echo 'b--->: ini var'
+my_vm_name=default
+#share_path_name=vm-ymc2-home-gitlab
+#share_path_in_win7=/f/docker-machine-data/default/ymc2/home/gitlab
+#share_path_in_vm_default=/ymc2/home/gitlab
+#my_docker_compose_path=/f/docker-machine-share-data/default/gitlab/ver-v1.0.1/01
+echo 'e--->: ini var'
---
+++ src/start.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+
+THIS_FILE_PATH=$(cd `dirname $0`; pwd)
+source ${THIS_FILE_PATH}/sh-lib-vm.sh
+source ${THIS_FILE_PATH}/conf.sh
+
+one_key_connect
